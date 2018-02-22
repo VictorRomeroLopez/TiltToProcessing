69962b88741f3af71662475f06d3458fe448afb9
Aleix Azuela <aleix.azuela@enti.cat>
2018-02-22 12:42:58 +0100

    Create All Enemies At diferent Speed
    This is V.1, not done yet

--- NewSketch/build/source/NewSketch.java
+++ NewSketch/build/source/NewSketch.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class NewSketch extends PApplet {
+
 //TestMoviment Personatge
 
 //Zona de variables
@@ -21,9 +37,9 @@ int vectorY;
 int normalizedVectorX;
 int normalizedVectorY;
 //Zona de setup
-void setup()
+public void setup()
 {
-  fullScreen();
+  
   strokeWeight(5);
   playerX = width/2;
   playerY = height/2;
@@ -36,7 +52,7 @@ void setup()
 }
 
 //Zona de draw
-void draw(){
+public void draw(){
   background(255);
   fill(0,255,0);
   ellipse(playerX,playerY,playerRadiusX,playerRadiusY);
@@ -75,5 +91,15 @@ void draw(){
   enemy3Y += normalizedVectorY;
 }
 
-void mousePressed(){
+public void mousePressed(){
+}
+  public void settings() {  fullScreen(); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "NewSketch" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
